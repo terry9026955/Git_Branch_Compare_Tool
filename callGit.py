4df661fe0c0382766db334d58a9fa36375d707ee
terry <terry9026955@gmail.com>
2022-07-12 16:33:43 +0800

    You commit this branch.

--- callGit.py
+++ callGit.py
@@ -9,20 +9,22 @@ def callGit():
 
     
     print("\n\n\n")
-    # Print SHA
+    
     print("Your remote SHA: ")
-    remoteSHA = subprocess.call("git rev-parse origin")  # To get the latest commit on the remote
+    subprocess.call("git rev-parse origin")  # To get the latest commit on the remote
     print("Your local SHA: ")
-    localSHA = subprocess.call("git rev-parse HEAD")           # To get the latest commit on the local
+    subprocess.call("git rev-parse HEAD") # To get the latest commit on the local
 
     
-    print("\nChecking branch...\n")
+    # 目前要想辦法讀取出subprocess.call()秀出來的內容
+    # print("\nChecking branch...\n")
     # Check branch of 2 side
-    if((remoteSHA) == (localSHA)):
-        print("Remote and Loacal are same branch.")
-    else:
-        print("Remote and Loacal are different branch.")
+    # if((remoteSHA) == (localSHA)):
+    #     print("Remote and Loacal are same branch.")
+    # else:
+    #     print("Remote and Loacal are different branch.")
 
+    
 
 def main():
     callGit()
