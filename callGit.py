685a84b691da6c9b847e7814f76545c8757d9828
terry <terry9026955@gmail.com>
2022-07-12 16:37:31 +0800

    You commit this branch.

--- callGit.py
+++ callGit.py
@@ -7,16 +7,14 @@ def callGit():
     subprocess.call("git commit -am \"You commit this branch.\"")
     subprocess.call("git push")
 
-    
-    print("\n\n\n")
 
-    print("Your remote SHA: ")
+    print("\nYour remote SHA: ")
     subprocess.call("git rev-parse origin")  # To get the latest commit on the remote
-    print("Your local SHA: ")
+    print("\nYour local SHA: ")
     subprocess.call("git rev-parse HEAD") # To get the latest commit on the local
 
     
-    
+
     # 目前要想辦法讀取出subprocess.call()秀出來的內容，不然他現在只會返回0/1
     # print("\nChecking branch...\n")
     # Check branch of 2 side
