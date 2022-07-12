a54076bd76e6719658742a2e49a3aec923ccb5f8
terry <terry9026955@gmail.com>
2022-07-12 15:41:48 +0800

    You commit this branch.

--- callGit.py
+++ callGit.py
@@ -7,6 +7,10 @@ def callGit():
     subprocess.call("git commit -am \"You commit this branch.\"")
     subprocess.call("git push")
 
+    # Print SHA
+    subprocess.call("git rev-parse origin/master")  # To get the latest commit on the remote
+    subprocess.call("git rev-parse HEAD")           # To get the latest commit on the local
+
 
 def main():
     callGit()
