b70d55de01163f66355e51e7bd306a19d05ccd30
terry <terry9026955@gmail.com>
2022-07-22 15:43:22 +0800

    修到出BUG版

--- project_intel_smoke_test/test_plan_ui_origin.py
+++ project_intel_smoke_test/test_plan_ui_origin.py
@@ -13,12 +13,15 @@ import logging
 import time
 from functools import partial
 
+# Default branch: Echoharbor_N38A_Main
 
 
+# Counter for recursion times recording and restarting program
+counter = 0
 
 
 class Main(QMainWindow, ui.Ui_MainWindow):
-    SHA = None
+    SHA = None  # 全域
     stopmissionFlag = False
     version_number = "20220718A_BETA"
     # determine if application is a script file or frozen exe
@@ -59,6 +62,8 @@ class Main(QMainWindow, ui.Ui_MainWindow):
         ini = self.loadini()
         folderPath = ini.value("Folder/path")
         gitPath = ini.value("Git_path/path")
+        print("getInfo called!") # TEST
+        print("gitPath: " , gitPath)
         
         # get number of list 
         listNumber = self.listWidget.count()
@@ -74,15 +79,16 @@ class Main(QMainWindow, ui.Ui_MainWindow):
         
     
     def threadRunSHA(self, folderpath, listWidget_count, scriptList, branch, gitPath):
-        
-        self.thread = QThread(parent=self)
+        print("threadRunSHA called!")
+        print("now the branch is " + branch)
+        self.thread = QThread(parent=self)  # 開新Thread
         
         self.worker = getSHA()
         # Move worker to the thread
         self.worker.moveToThread(self.thread)
         # Connect signals and slots
         
-        self.thread.started.connect(partial(self.worker.main, gitPath, branch))
+        self.thread.started.connect(partial(self.worker.main, gitPath, branch)) # 迴圈跳來會重新開一個新Thread做Thread2的事情
 
         # 當收到finished, 線程結束
         self.worker.finished.connect(self.thread.quit)
@@ -579,7 +585,7 @@ class getSHA(QThread):
             return False
         else:
             print("【Remote】 and 【Loacal】 are \'different\' branch.")
-            self.gitPull()
+            #self.gitPull()
 
         # return SHA
         self.writeSHA(real_time, remoteSHA, localSHA)
@@ -620,6 +626,7 @@ class getSHA(QThread):
 
 
     def gotoPath(self, gitPath):
+        print("now in the gotoPath of getSHA")
         os.chdir(gitPath)
         # os.chdir('D:/SourceCode_SM2269')
         cwd = os.getcwd() 
@@ -636,24 +643,46 @@ class getSHA(QThread):
             for line in logInfo:
                 file.write(line)
                 file.write("\n")
+        
+        # 抓出關鍵字
         with open("log.txt", "r") as read:
-            readfile = read.readlines()[19]
+            for line in logInfo:
+                if "Author" in line:
+                    name = line.split(" ")  # 切成List
+                    fullname = str(name[1])
+                    if "<" not in name[2]:
+                        fullname = name[1] + name[2]
+                    print("Author: ", fullname)
+                    
             
-        print("Author: " + readfile)
+            #readfile = read.readlines()[19]
+            
+        #print("Author: " + readfile)
         
         return True
         
         
     def main(self, gitPath, branch):
         try:
+            #counter for recording revursion
+            if counter >= 1000:
+                #重開機
+                #restart()   # 重啟?!?!
+                pass
+            else:
+                counter += 1 
+            
             real_time = time.strftime("%Y%m%d_%H%M%S", time.localtime())
             # Go to directory (git repository)
+            print("calling gotoPath...")
+            print("gitPath:", gitPath)
             self.gotoPath(gitPath)
+            print("After gotoPath calling...")
             # showFile()
             # gitCheck()
 
             # Get SHA
-            compareTigger = self.checkBranch(real_time, branch)
+            compareTigger = self.checkBranch(real_time, branch) # 注意版本要打對(在ini裡面改即可)
             
             if compareTigger == True:
                 tiggerList = self.gitLog()
@@ -738,8 +767,9 @@ class runBatchcommand(QThread):
 
                                 procress = subprocess.run([file_path, branch_input, SHA_input])
 
-                                # if self.checkBox.isChecked():
-                                #     command = use_inbox_delete_smi_driver_tp.main()
+                                if self.checkBox.isChecked():
+                                    
+                                    command = use_inbox_delete_smi_driver_tp.main()
                                     
 
                             elif "Restart" in file_path or "restart" in file_path:
@@ -793,6 +823,9 @@ class runBatchcommand(QThread):
 
         
 if __name__ == '__main__':
+    # 防止程式整個崩潰掉
+    sys.setrecursionlimit(100000)  
+    
     app = QtWidgets.QApplication(sys.argv)
     window = Main()
     window.show()
