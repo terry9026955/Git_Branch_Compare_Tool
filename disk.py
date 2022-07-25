84df11061feea4b0e9adceac76756b5fcd89871f
terry <terry9026955@gmail.com>
2022-07-25 19:20:46 +0800

    Get disk ID info

---
+++ disk.py
@@ -0,0 +1,30 @@
+# get disk ID
+# 注意檔名不要取的跟指令名字一樣
+
+from multiprocessing.connection import Client
+import subprocess
+from subprocess import run
+from subprocess import Popen, PIPE
+import os
+import time
+
+def main(): 
+    #os.system('cmd /k "diskpart"')  
+    # try:
+    #     os.system('cmd /k "diskpart"')
+    #     os.system('cmd /k "list disk"')
+
+
+    # except:
+    #     print('could not execute command')
+    
+    # command = "diskpart"
+    # run(command, shell=True)
+    p = Popen(["diskpart"], stdin=PIPE)
+    print("sending data to STDIN")
+    
+    res1 = p.stdin.write(bytes("list disk\n", 'utf-8'))
+    time.sleep(.5)
+
+if __name__ == "__main__":
+    main()
