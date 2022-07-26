2d1cd1cca606f087489fe966100b9e0f664ddaa3
terry <terry9026955@gmail.com>
2022-07-26 12:50:30 +0800

    處理解碼

--- disk.py
+++ disk.py
@@ -4,20 +4,43 @@
 
 from multiprocessing.connection import Client
 import subprocess
-from subprocess import run
 from subprocess import *
 import os
 import time
+import sys
 
 def main(): 
-    # 先開diskpart
-    p = Popen(["diskpart"], stdin=PIPE)
-    print("sending data to STDIN")
-    
+    # 先開diskpart (加了stdout就不會自動整個印出來在terminal上面)
+    p = Popen(["diskpart"], stdin=PIPE, stdout=PIPE)
     # 再下list disk去看disk ID
-    res1 = str(p.stdin.write(bytes("list disk\n", 'utf-8')))
-    time.sleep(.5)
+    p.stdin.write(bytes("list disk\n", 'utf-8'))
     
+    
+    stdout = str(p.communicate())
+    #print(stdout)
+    
+    with open("temp.txt", "w") as wfile:
+        stdout.split("\\n")
+        wfile.write(stdout)
+        
+    with open("temp.txt", "r") as rfile:
+        print(rfile)
+        
+        
+    # 解碼:
+    s = "\xba\xcf\xba\xd0 0"
+    
+    
+    
+
+    # with open("temp.txt", "wt") as file:
+    #     file.write("command\ncommand\ncommand")
+    # p = Popen(["diskpart", "/?", bytes("list disk".encode("GBK")), "temp.txt"], stdin=PIPE, )
+    #os.remove("temp.txt")
+    
+
+    
+
 if __name__ == "__main__":
     main()
     
