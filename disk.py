18fc393e7b4165c9de2f9559cef13662426e38ac
terry <terry9026955@gmail.com>
2022-07-28 10:42:29 +0800

    get disk ID info

--- disk.py
+++ disk.py
@@ -11,6 +11,8 @@ import sys
 import re
 from turtle import st
 
+
+
 def get_disk_info(): 
     # 先開diskpart (加了stdout就不會自動整個印出來在terminal上面)
     p = Popen(["diskpart"], stdin=PIPE, stdout=PIPE)
@@ -22,8 +24,7 @@ def get_disk_info():
     # Write list disk information to txt for recording
     with open("temp.txt", "w") as wfile:
         wfile.write(stdout)
-           
-            
+             
     # Get all disk size
     with open("temp.txt", "r") as rfile2:
         content = rfile2.readline().split("\\r\\n")
@@ -43,13 +44,13 @@ def get_disk_info():
                         #print("Disk: ", newItem[1], "Compacity: ", newItem[3])
                         if int(newItem[3]) < 2000:
                             print("PW ID", "Disk: ", newItem[1], "Compacity: ", newItem[3])
-
-
+                            
+# Show 'list disk' in terminal
 def only_show_disk():
     p = Popen(["diskpart"], stdin=PIPE)
     p.stdin.write(bytes("list disk\n", 'utf-8'))
 
-
+# get disk ID of online disk
 def diskDrive():
     #r = subprocess.run("wmic diskdrive get model,Name", stdout=subprocess.PIPE)
     r = str(subprocess.check_output("wmic diskdrive get model,Name"))
