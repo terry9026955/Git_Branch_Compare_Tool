32fa92e1b22f464f9a91336e556bb2f3cf2826c8
terry <terry9026955@gmail.com>
2022-07-29 16:29:18 +0800

    get fail log and error code

--- check_fail_log.py
+++ check_fail_log.py
@@ -2,43 +2,33 @@
 import os
 from re import T
 
-path = 'D:\Tinghao.Chen\Desktop\WeeklyScriptList_Intel_EH_3.sl_0001'
 
-for filename in os.listdir(path):
-    with open(os.path.join(path, filename), 'r') as f:
-        text = f.read() # 把當前路徑中所有檔案裡面所有的文字內容全存起來
-        # print(text)   # 全部檔案內容印出來
-        
-        if '.dsf Fail' in text:
-            #print('Fail log info: ', text) # 會把出錯的那整個file內容印出來
-            text = text.split('\n')         # 切成list
-            for line in text:
-                if '.dsf Fail' in line:
-                    print('Fail log info: ', line)  # 印出全部檔案當中出錯的那一行資訊(檔名與時間)
+def get_fail_log():
+    # 檔案路徑(可能會變動)
+    path = 'D:\Tinghao.Chen\Desktop\WeeklyScriptList_Intel_EH_3.sl_0001'
 
-
-
-
-
-# # 路徑下的所有檔名
-# filename = os.listdir(path)
-# print(filename[0])
-
-# with open(path+'\WL_FG_FwDL_Commit_ForEH.log', 'r') as read:
-#     readFlag = False
-#     for i in read:
-#         # 印出整段結尾錯誤訊息
-#         # if readFlag == True:  
-#         #     print(i)
-#         if '.dsf Fail' in i and readFlag == False:
-#             print('Fail log info: ', i)
-#             readFlag = True
-    
-        
-        
+    # for loop 掃全部檔名
+    for filename in os.listdir(path):
+        with open(os.path.join(path, filename), 'r') as f:
+            text = f.read() # 把當前路徑中所有檔案裡面所有的文字內容全存起來
+            # print(text)   # 全部檔案內容印出來
+            
+            if '.dsf Fail' in text:
+                # print(text) # 會把出錯的那整個file內容印出來
+                text = text.split('\n')         # 切成list
+                readFlag = False
+                for line in text:
+                    if readFlag == True:  
+                        print(line)
+                    elif '.dsf Fail' in line and readFlag == False:
+                        print('Fail log info: \n', line)  # 印出全部檔案當中出錯的那一行資訊(檔名與時間)
+                        readFlag = True
+                    elif '                  ' in line:
+                        readFlag == False
+                
 
 
 
-# 抓最新時間
-# 抓文件.dsf Fail
-# for loop 開檔案
+if __name__ == "__main__":
+    get_fail_log()
+
