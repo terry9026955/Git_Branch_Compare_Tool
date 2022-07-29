6c4c0f6d77c7d74921fdd8495e79c28eecb00472
terry <terry9026955@gmail.com>
2022-07-29 14:56:15 +0800

    scan whole file and get keyword line

--- check_fail_log.py
+++ check_fail_log.py
@@ -1,11 +1,44 @@
 # get fail log info
+import os
+from re import T
 
 path = 'D:\Tinghao.Chen\Desktop\WeeklyScriptList_Intel_EH_3.sl_0001'
 
-with open(path+'\DataGC_BG_DstBlk_ErsFail_S2T.log', 'r') as read:
-    cnt = 0
-    for i in read:
-        if cnt < 10:
-            print(i)
-            cnt += 1
+for filename in os.listdir(path):
+    with open(os.path.join(path, filename), 'r') as f:
+        text = f.read() # 把當前路徑中所有檔案裡面所有的文字內容全存起來
+        # print(text)   # 全部檔案內容印出來
+        
+        if '.dsf Fail' in text:
+            #print('Fail log info: ', text) # 會把出錯的那整個file內容印出來
+            text = text.split('\n')         # 切成list
+            for line in text:
+                if '.dsf Fail' in line:
+                    print('Fail log info: ', line)  # 印出全部檔案當中出錯的那一行資訊(檔名與時間)
+
+
+
+
+
+# # 路徑下的所有檔名
+# filename = os.listdir(path)
+# print(filename[0])
+
+# with open(path+'\WL_FG_FwDL_Commit_ForEH.log', 'r') as read:
+#     readFlag = False
+#     for i in read:
+#         # 印出整段結尾錯誤訊息
+#         # if readFlag == True:  
+#         #     print(i)
+#         if '.dsf Fail' in i and readFlag == False:
+#             print('Fail log info: ', i)
+#             readFlag = True
     
+        
+        
+
+
+
+# 抓最新時間
+# 抓文件.dsf Fail
+# for loop 開檔案
